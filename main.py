9e97daa08314c8d06399b9bae128441a09ca1aa3
matheus1714 <matheussilvamartins1714@gmail.com>
2022-10-07 15:44:48 -0300

    create main file

---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import dotenv_values
+
+from data.create_measure_file import create_measure_file
+from data.generate_measures import generate_measures
+
+from src.get_data_file import get_data_file
+
+if __name__ == '__main__':
+
+    config = dotenv_values(".dev.env")
+
+    file_path = config.get('DATA_FILE_PATH')
+    quant_measure = int(config.get('QUANT_MEASURES'))
+    measure_mean = float(config.get('MEASURE_MEAN'))
+    measure_std = float(config.get('MEASURE_STD'))
+
+    measures = generate_measures(quant_measure, measure_mean, measure_std)
+
+    create_measure_file(file_path, measures)
+
+    data = get_data_file(file_path)
+    
+
